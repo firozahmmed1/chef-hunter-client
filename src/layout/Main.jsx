7734876f56e53e220df7477809ba556016505edb
firozahmmed1 <firozahmmed05@gmail.com>
2023-05-02 14:39:22 +0600

    fifth-update

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from '../components/shared/Header/Header';
+import Footer from '../components/shared/Footer/Footer';
+
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
+            <div> 
+               
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>Homepage</h1>
+             <div className="carousel container">
+                    <div className="carousel-item h-3/6 w-full">
+                        <img src="https://www.vasterad.com/themes/chow_html/images/sliderA_01.jpg" alt="Burger" />
+                    </div>
+                    
+                </div> 
+         <h1>Homepage</h1>
         </div>
     );
 };
